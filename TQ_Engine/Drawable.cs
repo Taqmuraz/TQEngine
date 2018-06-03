4679ceb1efc523f763ce387ed68c8d90111ec636
Taqmuraz <lulook23@gmail.com>
2018-06-04 01:55:49 +0400

    INIT

---
+++ TQ_Engine/Drawable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using AnimalsSolution;
+using System.Windows.Forms;
+using System.Drawing;
+
+public class Slot : IncBehaviour {
+
+	public PictureBox picture { get; private set; }
+	public RectangleTrans trans { get; private set; }
+	public Bitmap[] pictures { get; private set; }
+
+	public Slot (Vector position, Vector size, string drawableName) {
+		trans = new RectangleTrans (position, size);
+		picture = new PictureBox ();
+
+		base.Init ();
+	}
+}
+
---
+++ TQ_Engine/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using System.Drawing;
+
+namespace AnimalsSolution
+{
+	class MainClass
+	{
+		public static GameScreen screen = new GameScreen();
+		public static Thread thread;
+		public static void Main (string[] args)
+		{
+			
+			thread = new Thread (GameScreen.ThreadUpdate);
+			thread.Start ();
+			Application.Run (screen);
+		}
+	}
+
+	public class GameScreen : Form
+	{
+		
+
+		public GameScreen () {
+			Game_0.game_0 = new Game_0 ();
+
+			TextBox text = new TextBox ();
+
+			text.SetBounds (100, 100, 100, 50);
+		}
+
+		public static void ThreadUpdate () {
+
+			Game_0.game_0.Start ();
+
+			while (true) {
+				MainClass.screen.Update ();
+			}
+		}
+
+		public static int width
+		{
+			get
+			{
+				if (MainClass.screen != null) {
+					return MainClass.screen.Width;
+				} else {
+					return 0;
+				}
+			}
+		}
+		public static int height
+		{
+			get
+			{
+				if (MainClass.screen != null) {
+					return MainClass.screen.Height;
+				} else {
+					return 0;
+				}
+			}
+		}
+		public static Vector mousePosition
+		{
+			get {
+				return (PointF)MainClass.screen.PointToClient (MousePosition);
+			}
+		}
+	}
+}
